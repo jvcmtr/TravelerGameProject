b80e746594317d75e14a5530cf9697774e803af7
jvcmtr <jvictorcmtr@gmail.com>
2023-08-28 20:03:06 -0300

    added new layout component (fadeToBlack)

---
+++ viteProcess/src/components/layout/fadeToBlack.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function FadeToBlack(props){
+
+    const Dark = React.useState(props.Dark)
+
+  return(
+    <div 
+      style={{
+        zIndex: '10',
+        x: '0',
+        y:'0',
+        position: 'fixed',
+        height:"100vh",
+        width:"100vw",
+        backgroundColor: "#000",
+        opacity: Dark? 0 : 1,
+        transitionDuration: '1s',
+    }}  
+    >
+        {props.children}
+    </div>
+  )
+}
